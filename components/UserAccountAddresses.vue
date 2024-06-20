e8e721b4f0e8c399f67534d4f966dcc547b3e97e
zeiadsalhin <zeyadali011@gmail.com>
2024-06-20 07:00:54 +0300

    v1

--- components/UserAccountAddresses.vue
+++ components/UserAccountAddresses.vue
@@ -10,8 +10,9 @@ const loading2 = ref(false)
 const userAddress = ref([]);
 const usernewAddress = ref([]);
 const editMode = ref(false);
-const editedAddress = ref({});
+const editedAddress = ref();
 const newaddress = ref(false)
+const loadingAddresses = ref(true)
 // Fetch user's address on component mount
 onMounted(() => {
     fetchAddresses();
@@ -30,7 +31,9 @@ const fetchAddresses = async () => {
         }
         if (data) {
             userAddress.value = [data]; // Store fetched address in an array
-            editedAddress.value = { ...userAddress.value }; // Initialize editedAddress with user's address
+            editedAddress.value = userAddress.value; // Initialize editedAddress with user's address
+            // console.log(userAddress.value);
+            loadingAddresses.value = false
         }
     } catch (error) {
         console.error('Error fetching user address:', error.message);
@@ -41,21 +44,36 @@ const newaddresstrigger = () => {
     newaddress.value = true
     editAddress()
 }
-const editAddress = (address) => {
+
+
+
+// begin edit address
+const id = ref();
+const updateValue = (addressId) => {
+    id.value = addressId;
+    // console.log('id value', id.value);
+    editAddress()
+};
+const editAddress = () => {
     editMode.value = true;
 
     if (newaddress.value == false) {
-        editedAddress.value = { ...address }; // Copy the address to editedAddress
-        console.log(editedAddress.value);
+        userAddress.value = editedAddress.value[0][id.value];
+        // userAddress.value = {  }
+        // console.log('editing User address:', editedAddress.value[0][id.value]);
     } else {
-        editedAddress.value = { ...usernewAddress.value[0] };
+        userAddress.value = []
+        usernewAddress.value = userAddress.value;
+        // console.log('una', usernewAddress.value);
     }
 };
 
 // Function to cancel edit mode and reset editedAddress
 const cancelEdit = () => {
     editMode.value = false;
-    editedAddress.value = {}; // Reset editedAddress
+    editedAddress.value = []; // Reset editedAddress
+    userAddress.value = []
+    fetchAddresses()
 };
 
 // Function to save the edited, new addresses
@@ -67,14 +85,11 @@ const saveAddress = async () => {
         const { data, error } = await supabase.auth.getSession();
         const userId = data.session.user.id;
 
-        // Update user's address with editedAddress
-        userAddress.address = { ...editedAddress.value };
-
         try {
             // Save the edited address to Supabase
             const { data2, error } = await supabase
                 .from('user_address')
-                .upsert([{ uid: userId, ...editedAddress.value }]);
+                .upsert([{ uid: userId, ...editedAddress.value[0][id.value] }]);
             // , { onConflict: ['uid'] }
             if (error) {
                 throw error;
@@ -93,19 +108,17 @@ const saveAddress = async () => {
         const { data, error } = await supabase.auth.getSession();
         const userId = data.session.user.id;
 
-        // Add user's address with editedAddress
-        usernewAddress.address = { ...editedAddress.value };
-
         try {
             // Save the new address to Supabase
             const { data2, error } = await supabase
                 .from('user_address')
-                .upsert([{ uid: userId, ...editedAddress.value }]);
+                .upsert([{ uid: userId, ...usernewAddress.value }]);
             // , { onConflict: ['uid'] }
             if (error) {
                 throw error;
             }
             fetchAddresses()
+            // console.log('address to add', usernewAddress.value);
             // console.log('Address saved:', data2);
         } catch (error) {
             console.error('Error saving address:', error.message);
@@ -119,7 +132,8 @@ const saveAddress = async () => {
 </script>
 <template>
     <div class="mb-20">
-        <h2 class="text-2xl font-semibold mb-10">Manage Addresses</h2>
+        <v-icon class="m-2" size="30">mdi-home</v-icon>
+        <h2 class="text-2xl font-bold mb-10">Manage Delivery Addresses</h2>
 
         <div v-if="editMode && editedAddress">
             <form id="form" class="space-y-5 p-5 w-11/12 md:w-11/12 h-fit text-center mx-auto justify-center flex-col"
@@ -130,7 +144,7 @@ const saveAddress = async () => {
                         class="text flex w-full ">
                         <label for="street" class="p-2 font-semibold text-lg text-left my-auto">Street<span
                                 class="required text-red-600">*</span>:</label>
-                        <input id="street" name="street" v-model="editedAddress.street"
+                        <input id="street" name="street" v-model="userAddress.street"
                             placeholder="Street name, address..." type="text" @focus="move" @blur="back"
                             class=" outline outline-1 outline-zinc-700 rounded-lg  px-2  text-black bg-transparent p-3 w-full"
                             :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -141,7 +155,7 @@ const saveAddress = async () => {
                             class="text md:flex w-full text-left  mx-auto">
                             <label for="street" class="p-2 font-semibold text-lg my-auto">Building<span
                                     class="required text-red-600">*</span>:</label>
-                            <input id="building" name="building" v-model="editedAddress.building"
+                            <input id="building" name="building" v-model="userAddress.building"
                                 placeholder="Building no." type="text" @focus="move" @blur="back"
                                 class=" outline outline-1 px-2 outline-zinc-700 rounded-lg text-black bg-transparent p-3 w-full"
                                 :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -150,7 +164,7 @@ const saveAddress = async () => {
                             class="text md:flex  w-full text-left mx-auto">
                             <label for="street" class="p-2 font-semibold text-lg my-auto">Apartment<span
                                     class="required text-red-600">*</span>:</label>
-                            <input id="apartment" name="apartment" v-model="editedAddress.apartment"
+                            <input id="apartment" name="apartment" v-model="userAddress.apartment"
                                 placeholder="Apartment, flat no." type="text" @focus="move" @blur="back"
                                 class="outline outline-1 px-2 outline-zinc-700 rounded-lg  text-black bg-transparent p-3 w-full"
                                 :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -161,7 +175,7 @@ const saveAddress = async () => {
                         class="text md:flex   w-full text-left mx-auto">
                         <label for="street" class="p-2 font-semibold text-lg my-auto">Neighborhood<span
                                 class="required text-red-600">*</span>:</label>
-                        <input id="neighborhood" name="neighborhood" v-model="editedAddress.neighborhood"
+                        <input id="neighborhood" name="neighborhood" v-model="userAddress.neighborhood"
                             placeholder="Neighborhood or district" type="text" @focus="move" @blur="back"
                             class="outline outline-1 px-2 outline-zinc-700 rounded-lg  text-black  bg-transparent  p-3 w-full"
                             :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -169,20 +183,19 @@ const saveAddress = async () => {
                     <div class="5,6,7 grid md:grid-cols-3 grid-rows-3 md:grid-rows-1  gap-4 w-full">
                         <div :class="theme.global.current.value.dark ? ' text-white' : 'bg-zinc-100 text-black'"
                             class="text  outline outline-1 px-2 outline-zinc-700 rounded-lg  w-full ">
-                            <input id="city" name="city" v-model="editedAddress.city" placeholder="City*" type="text"
+                            <input id="city" name="city" v-model="userAddress.city" placeholder="City*" type="text"
                                 @focus="move" @blur="back" class="text-black outline-none bg-transparent  p-3 w-full"
                                 :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
                         </div>
                         <div :class="theme.global.current.value.dark ? ' text-white' : 'bg-zinc-100 text-black'"
                             class="text  outline outline-1 px-2 outline-zinc-700 rounded-lg  w-full ">
-                            <input id="state" name="state" v-model="editedAddress.state" placeholder="State*"
-                                type="text" @focus="move" @blur="back"
-                                class="text-black outline-none bg-transparent  p-3 w-full"
+                            <input id="state" name="state" v-model="userAddress.state" placeholder="State*" type="text"
+                                @focus="move" @blur="back" class="text-black outline-none bg-transparent  p-3 w-full"
                                 :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
                         </div>
                         <div :class="theme.global.current.value.dark ? ' text-white' : 'bg-zinc-100 text-black'"
                             class="text  outline outline-1 px-2 outline-zinc-700 rounded-lg  w-full ">
-                            <input id="postcode" name="postcode" v-model="editedAddress.postalCode"
+                            <input id="postcode" name="postcode" v-model="userAddress.postalCode"
                                 placeholder="Post code*" type="number" min="4" @focus="move" @blur="back"
                                 class="text-black outline-none bg-transparent  p-3 w-full"
                                 :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -194,7 +207,7 @@ const saveAddress = async () => {
                         class="text md:flex  w-full text-left mx-auto">
                         <label for="street" class="p-2 font-semibold text-lg my-auto">Country<span
                                 class="required text-red-600">*</span>:</label>
-                        <input id="country" name="country" v-model="editedAddress.country" placeholder="Country"
+                        <input id="country" name="country" v-model="userAddress.country" placeholder="Country"
                             type="text" @focus="move" @blur="back"
                             class="outline outline-1 px-2 outline-zinc-700 rounded-lg  text-black outline-none bg-transparent text-lg p-3 w-full"
                             :class="theme.global.current.value.dark ? 'text-white' : 'text-black'" required>
@@ -220,22 +233,43 @@ const saveAddress = async () => {
             </form>
         </div>
 
-        <div v-else class="md:flex justify-center md:space-x-4 text-left text-lg shadow-md w-full p-5 mx-auto">
-            <div v-if="userAddress" v-for="(address, index) in userAddress[0]" :key="index"
-                class="space-y-5 my-5 w-fit p-5 outline outline-zinc-700 shadow-md">
-                <strong>Street:</strong> {{ address.street }} <br>
-                <strong>Building:</strong> {{ address.building }} <br>
-                <strong>Apartment:</strong> {{ address.apartment }} <br>
-                <strong>Neighborhood:</strong> {{ address.neighborhood }} <br>
-                <strong>City:</strong> {{ address.city }} <br>
-                <strong>State:</strong> {{ address.state }} <br>
-                <strong>Postal Code:</strong> {{ address.postalCode }} <br>
-                <strong>Country:</strong> {{ address.country }} <br>
-                <v-btn @click="editAddress" class="ma-5 flex w-1/3 justify-center" min-height="40" min-width="150"
-                    color="grey-lighten-2">Edit</v-btn>
+        <div v-else>
+            <div v-if="!loadingAddresses"
+                class="md:grid grid-cols-2 gap-2 justify-center md:space-x-  text-left text-lg shadow-md w-fit p-5 mx-auto">
+                <div v-if="userAddress" v-for="(address, index) in userAddress[0]" :key="index"
+                    class=" mb-2 w-fit p-5 outline outline-zinc-700 shadow-lg">
+
+                    <strong>Street:</strong> {{ address.street }} <br>
+                    <strong>Building:</strong> {{ address.building }} <br>
+                    <strong>Apartment:</strong> {{ address.apartment }} <br>
+                    <strong>Neighborhood:</strong> {{ address.neighborhood }} <br>
+                    <strong>City:</strong> {{ address.city }} <br>
+                    <strong>State:</strong> {{ address.state }} <br>
+                    <strong>Postal Code:</strong> {{ address.postalCode }} <br>
+                    <strong>Country:</strong> {{ address.country }} <br>
+                    <div class="grid grid-cols-2 grid-rows-2 w-11/12 mx-auto text-center justify-center space-x- mt-4">
+                        <v-btn @click="updateValue(index)" class="m-1" min-height="40" max-width="100"
+                            color="grey-lighten-2">Edit</v-btn>
+                        <v-btn @click="" class="m-1" min-height="40" max-width="100" color="red-darken-2">Delete</v-btn>
+                        <v-btn @click="" class="m-2 col-span-2 w-fuasll flaex justify-center" min-height="40"
+                            max-width="200" variant="outlined">Set as
+                            default</v-btn>
+                    </div>
+                </div>
+                <div v-else>No address available for this user.</div>
             </div>
-            <div v-else>No address available for this user.</div>
+            <div v-else>
+                <div class="flex justify-center w-full">
+                    <div class=" shadow-sm">
+                        <div class="flex w-full justify-center text-zinc-950 p-10"><v-progress-circular
+                                color="dark-blue" indeterminate></v-progress-circular>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
+
 
         <div v-if="!editMode" class="add p-5">
             <v-btn @click="newaddresstrigger"><v-icon>mdi-plus</v-icon>Add New
